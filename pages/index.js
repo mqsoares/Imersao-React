c2d114fabb4e7a43a512d79eccaf5ab058b0e128
mqsoares <mqseraos@gmail.com>
2022-12-12 14:50:00 -0300

    Add theme dark and light

--- pages/index.js
+++ pages/index.js
@@ -1,75 +1,90 @@
 import React from "react";
-import myconfig from "../myconfig.json";
+import config from "../myconfig.json";
 import styled from "styled-components";
 import Menu from "../src/components/Menu";
-import { CSSreset } from "../src/components/CSSreset";
 import { StyledTimeline } from "../src/components/TimeLine";
 
-function HomePage(){
+function HomePage() {
     const [valorDoFiltro, setValorDoFiltro] = React.useState("");
 
     return (
         <>
-            <CSSreset/>
-            <div>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 1,
+                // backgroundColor: "red",
+            }}>
+                {/* Prop Drilling */}
                 <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
-                <Header/>
-                <TimeLine searchValue={valorDoFiltro} playlists={myconfig.playlists}/>
+                <Header />
+                <Timeline searchValue={valorDoFiltro} playlists={config.playlists}>
+                    Conteúdo
+                </Timeline>
             </div>
         </>
     );
 }
-
 export default HomePage
+// function Menu() {
+//     return (
+//         <div>
+//             Menu
+//         </div>
+//     )
+// }
 
-const StyledHeader = styled.section`
+
+const StyledHeader = styled.div`
+    background-color: ${({ theme }) => theme.backgroundLevel1};
     img {
-        width: 1512px;
-        height: 230px;    
-    }
-
-    .userInfor {
-        display: flex;
-        align-items: center;
-        gap: 11px;
-    }
-    
-    .userInfor img {
         width: 80px;
         height: 80px;
         border-radius: 50%;
     }
-    
-    .userInforTxt h1, p{
-        margin: 0;
+    .user-info {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
     }
-`
-const StyledBanner = styled.div`
-    background-image: url(${myconfig.banner});
-    height: 250px;
 `;
-
-function Header(){
+const StyledBanner = styled.div`
+    background-color: blue;
+    background-image: url(${({ bg }) => bg});
+    /* background-image: url(${config.bg}); */
+    height: 230px;
+`;
+function Header() {
     return (
         <StyledHeader>
-            <StyledBanner/>
-            <div className="userInfor">
-                <img src={`https://github.com/${myconfig.github}.png`}></img>
-                <div className="userInforTxt">
-                    <h1>{myconfig.name}</h1>
-                    <p>{myconfig.job}</p>
+            <StyledBanner bg={config.bg} />
+            <section className="user-info">
+                <img src={`https://github.com/${config.github}.png`} />
+                <div>
+                    <h2>
+                        {config.name}
+                    </h2>
+                    <p>
+                        {config.job}
+                    </p>
                 </div>
-            </div>
+            </section>
         </StyledHeader>
-    );
+    )
 }
-
-function TimeLine({ searchValue, ...props }){
-    const playlistNames = Object.keys(props.playlists)
-    return(
+function Timeline({ searchValue, ...propriedades }) {
+    // console.log("Dentro do componente", propriedades.playlists);
+    const playlistNames = Object.keys(propriedades.playlists);
+    // Statement
+    // Retorno por expressão
+    return (
         <StyledTimeline>
-            {playlistNames.map((playlistName)=>{
-                const videos = props.playlists[playlistName]
+            {playlistNames.map((playlistName) => {
+                const videos = propriedades.playlists[playlistName];
+                // console.log(playlistName);
+                // console.log(videos);
                 return (
                     <section key={playlistName}>
                         <h2>{playlistName}</h2>
@@ -80,21 +95,20 @@ function TimeLine({ searchValue, ...props }){
                                     const searchValueNormalized = searchValue.toLowerCase();
                                     return titleNormalized.includes(searchValueNormalized)
                                 })
-                            
-                                .map((video)=> {
+                                .map((video) => {
                                     return (
-                                        <a key={video.url} href={video.url} target="_blank">
-                                            <img src={video.thumb}></img>
+                                        <a key={video.url} href={video.url}>
+                                            <img src={video.thumb} />
                                             <span>
                                                 {video.title}
-                                            </span>                
+                                            </span>
                                         </a>
-                                    )}
-                                )}
+                                    )
+                                })}
                         </div>
                     </section>
                 )
             })}
         </StyledTimeline>
-    );
+    )
 }
